cadf3454d534b09b362a9229364185d07481298e
annamelnyk <anna_melnyk@epam.com>
2023-05-08 22:57:44 +1000

    Work with router, filling detail view

--- APM-Start/src/app/product-view/product-view.component.ts
+++ APM-Start/src/app/product-view/product-view.component.ts
@@ -1,15 +1,36 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ProductListService } from '../shared/product-list.service';
+import { IProduct } from '../types';
 
 @Component({
   selector: 'pm-product-view',
   templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.css']
+  styleUrls: ['./product-view.component.css'],
 })
 export class ProductViewComponent implements OnInit {
+  product: any = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productListService: ProductListService
+  ) {
+    console.log();
   }
 
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    
+    this.productListService
+      .getProductList()
+      .subscribe(
+        (data: IProduct[]) =>
+          (this.product = data.find((item: IProduct) => item.productId === id))
+      );
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
